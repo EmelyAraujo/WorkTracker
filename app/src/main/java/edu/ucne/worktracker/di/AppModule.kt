f90d84916f46f1d66de8a9c5bd7e7e08bc58a8e3
HP <johanna261michael@gmail.com>
2023-04-07 12:55:52 -0400

    Agregando AppModule.kt

--- app/src/main/java/edu/ucne/worktracker/di/AppModule.kt
+++ app/src/main/java/edu/ucne/worktracker/di/AppModule.kt
@@ -1,11 +1,15 @@
 package edu.ucne.worktracker.di
 
+import android.content.Context
+import androidx.room.Room
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import edu.ucne.worktracker.data.local.WorkTrackerDb
 import edu.ucne.worktracker.data.remote.WorkTickerApi
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -15,19 +19,43 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
-    @Singleton
-    @Provides
-    fun providesMoshi(): Moshi {
-        return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-    }
 
-    @Singleton
-    @Provides
-    fun providesWorkTrackerApi(moshi: Moshi): WorkTickerApi {
-         return Retrofit.Builder()
-            .baseUrl("https://worktrackerapi1.azurewebsites.ne")
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .build()
-            .create(WorkTickerApi::class.java)
+    @Module
+    @InstallIn(SingletonComponent::class)
+    object AppModule {
+
+        @Singleton
+        @Provides
+        fun providesDatabase(@ApplicationContext context: Context): WorkTrackerDb {
+            return Room.databaseBuilder(
+                context,
+                WorkTrackerDb::class.java,
+                "TePresto.db"
+            )
+                .fallbackToDestructiveMigration()
+                .build()
+        }
+
+        @Singleton
+        @Provides
+        fun providesMaterialDao(db: WorkTrackerDb) = db.materialDao
+
+
+
     }
+    //  @Singleton
+    //  @Provides
+    //   fun providesMoshi(): Moshi {
+    //      return Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+    //  }
+
+    //  @Singleton
+    //  @Provides
+    //  fun providesWorkTrackerApi(moshi: Moshi): WorkTickerApi {
+    //       return Retrofit.Builder()
+    //          .baseUrl("https://worktrackerapi1.azurewebsites.ne")
+    //          .addConverterFactory(MoshiConverterFactory.create(moshi))
+    //          .build()
+    //          .create(WorkTickerApi::class.java)
+    //  }
 }
